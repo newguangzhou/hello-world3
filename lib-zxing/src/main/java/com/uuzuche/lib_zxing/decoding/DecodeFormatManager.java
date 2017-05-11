e98a8c8671cb1d85c66ae82e216b450d8c57b1a8
龙 <bingodongtian@gmail.com>
2017-05-11 19:08:01 +0800

    重构二维码扫描模块

--- lib-zxing/src/main/java/com/uuzuche/lib_zxing/decoding/DecodeFormatManager.java
+++ lib-zxing/src/main/java/com/uuzuche/lib_zxing/decoding/DecodeFormatManager.java
@@ -30,24 +30,27 @@ public class DecodeFormatManager {
 
     private static final Pattern COMMA_PATTERN = Pattern.compile(",");
 
-    public static final Vector<BarcodeFormat> PRODUCT_FORMATS;
+//    public static final Vector<BarcodeFormat> PRODUCT_FORMATS;
     public static final Vector<BarcodeFormat> ONE_D_FORMATS;
     public static final Vector<BarcodeFormat> QR_CODE_FORMATS;
     public static final Vector<BarcodeFormat> DATA_MATRIX_FORMATS;
 
     static {
-        PRODUCT_FORMATS = new Vector<BarcodeFormat>(5);
-        PRODUCT_FORMATS.add(BarcodeFormat.UPC_A);
-        PRODUCT_FORMATS.add(BarcodeFormat.UPC_E);
-        PRODUCT_FORMATS.add(BarcodeFormat.EAN_13);
-        PRODUCT_FORMATS.add(BarcodeFormat.EAN_8);
+//        PRODUCT_FORMATS = new Vector<BarcodeFormat>(5);
+//        PRODUCT_FORMATS.add(BarcodeFormat.UPC_A);
+//        PRODUCT_FORMATS.add(BarcodeFormat.UPC_E);
+//        PRODUCT_FORMATS.add(BarcodeFormat.EAN_13);
+//        PRODUCT_FORMATS.add(BarcodeFormat.EAN_8);
         // PRODUCT_FORMATS.add(BarcodeFormat.RSS14);
-        ONE_D_FORMATS = new Vector<BarcodeFormat>(PRODUCT_FORMATS.size() + 4);
-        ONE_D_FORMATS.addAll(PRODUCT_FORMATS);
-        ONE_D_FORMATS.add(BarcodeFormat.CODE_39);
-        ONE_D_FORMATS.add(BarcodeFormat.CODE_93);
+//        ONE_D_FORMATS = new Vector<BarcodeFormat>(PRODUCT_FORMATS.size()  + 4);
+
+                ONE_D_FORMATS = new Vector<BarcodeFormat>(1);
+
+//        ONE_D_FORMATS.addAll(PRODUCT_FORMATS);
+//        ONE_D_FORMATS.add(BarcodeFormat.CODE_39);
+//        ONE_D_FORMATS.add(BarcodeFormat.CODE_93);
         ONE_D_FORMATS.add(BarcodeFormat.CODE_128);
-        ONE_D_FORMATS.add(BarcodeFormat.ITF);
+//        ONE_D_FORMATS.add(BarcodeFormat.ITF);
         QR_CODE_FORMATS = new Vector<BarcodeFormat>(1);
         QR_CODE_FORMATS.add(BarcodeFormat.QR_CODE);
         DATA_MATRIX_FORMATS = new Vector<BarcodeFormat>(1);
@@ -88,9 +91,9 @@ public class DecodeFormatManager {
             }
         }
         if (decodeMode != null) {
-            if (Intents.Scan.PRODUCT_MODE.equals(decodeMode)) {
-                return PRODUCT_FORMATS;
-            }
+//            if (Intents.Scan.PRODUCT_MODE.equals(decodeMode)) {
+//                return PRODUCT_FORMATS;
+//            }
             if (Intents.Scan.QR_CODE_MODE.equals(decodeMode)) {
                 return QR_CODE_FORMATS;
             }
