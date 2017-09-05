2f6a2030f0b861180ad62afb4926ed069e6221f3
龙 <bingodongtian@gmail.com>
2017-09-05 17:24:56 +0800

    环境切换

--- app/src/main/java/com/xiaomaoqiu/now/Environment.java
+++ app/src/main/java/com/xiaomaoqiu/now/Environment.java
@@ -6,10 +6,19 @@ package com.xiaomaoqiu.now;
 
 public enum Environment {
     Debug(false),Release(true);
-    private Environment(boolean bugly_log){
+    private Environment(boolean release){
         this.bugly_log=bugly_log;
+        if(release){
+            bugly_appid="80761b13d7";
+        }else {
+            bugly_appid="5bd407f8b5";
+        }
+
     }
     //bugly是否打开log
     public boolean bugly_log;
+
+    //线上
+    public String bugly_appid="80761b13d7";
 
 }
